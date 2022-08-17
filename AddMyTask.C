9c8a01dc3170218bfc03dd7632e8b98323e195d9
Sergei <94015310+ssolokhi@users.noreply.github.com>
2022-08-18 00:58:08 +0300

    Update AddMyTask.C

--- AddMyTask.C
+++ AddMyTask.C
@@ -9,14 +9,14 @@ AliAnalysisTaskMyTask *AddMyTask(TString name = "name") {
 	if (!mgr || !mgr->GetInputEventHandler()) return 0x0;
 
 	TString fileName = AliAnalysisManager::GetCommonFileName();
-	fileName += ":MyTask"; // creates a subfolder
+	fileName += ":MyTask"; // creates a folder in the ROOT file
 
 	AliAnalysisTaskMyTask *task = new AliAnalysisTaskMyTask(name.Data());
 	if (!task) return 0x0;
 	mgr->AddTask(task);
 	task->SelectCollisionCandidates(AliVEvent::kINT7);
 	mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
-	mgr->ConnectOutput(task, 1, mgr->CreateContainer("MyOutputContainer", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+	mgr->ConnectOutput(task, 1, mgr->CreateContainer("MyOutputContainer", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data())); // name of the ROOT file output subfolder
 
 	return task;
 }
